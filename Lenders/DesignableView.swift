518f16742dd5d75652fcbb1e26fe845ce657525c
marcospojr <marcospojr13@gmail.com>
2020-08-27 22:41:34 -0300

    First commit

---
+++ Lenders/DesignableView.swift
@@ -0,0 +1,36 @@
+//
+//  DesignableView.swift
+//  Lenders
+//
+//  Created by Marcos Junior on 27/08/20.
+//  Copyright © 2020 Marcos Junior. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
+
---
+++ Lenders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lenders
+//
+//  Created by Marcos Junior on 27/08/20.
+//  Copyright © 2020 Marcos Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
